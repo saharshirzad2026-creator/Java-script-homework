13de2da1458dacbfd0380c6e551518255495d69a
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-03-11 18:18:53 -0700

    new file

---
+++ app.js
@@ -0,0 +1,30 @@
+export const list = [
+    {
+        name: "Sahil",
+        lastName: "Mohseni",
+        age: 26,
+    },
+    {
+        name: "Saliha",
+        lastName: "Amini",
+        age: 23,
+    },
+    {
+        name: "Sohel",
+        lastName: "Karimi",
+        age: 30,
+    },
+    {
+        name: "Sana",
+        lastName: "Zahidi",
+        age: 17,
+    },
+    {
+        name: "Shahnaz",
+        lastName: "Mosavi",
+        age: 32,
+    }
+];
+export default function greeting(){
+    console.log("Hello and good morning everyone");
+}
