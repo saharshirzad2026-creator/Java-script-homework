6a2beb1bb22e702557a83533e34abd4de66c1774
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-03-11 18:20:12 -0700

    new file

---
+++ index.js
@@ -0,0 +1,14 @@
+// import { list, person } from "./app.js";
+// import greeting from "./app.js";
+// list.map(personalbar=>{
+//     console.log(personalbar.name);
+// });
+// greeting();
+
+const person = {
+    name: "Rayan",
+    lastName: "rezaie",
+    age: 34,
+    gender: "Male",
+}
+const {name,lastName,age,gender} = person;
